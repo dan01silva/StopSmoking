494feeca3f7f5c9e7e5b927ec644114a4263090a
Ricardo Manholetti <ricardomanholetti@gmail.com>
2021-03-30 22:25:03 -0300

    Layout básico form01

--- src/app/form01/form01.page.ts
+++ src/app/form01/form01.page.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form01.page.scss'],
 })
 export class Form01Page implements OnInit {
+  public idadeuser;
+  public genuser;
+  public cityuser;
+
+  public user = {
+    nome: ' ',
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public saveidade() {
+    this.user.nome = this.idadeuser;
+  }
+  public savegen() {
+    this.user.nome = this.genuser;
+  }
+  public savecity() {
+    this.user.nome = this.cityuser;
+  }
+  
+
+  logForm() {
+    this.user
   }
 
 }
