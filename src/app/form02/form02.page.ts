25b99984a72e74f98fd5fd6d8bd5a1bcd0b99100
Gustavo Paulino <gpaulino31@hotmail.com>
2021-03-30 23:03:21 -0300

    Layout basico form02

--- src/app/form02/form02.page.ts
+++ src/app/form02/form02.page.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form02.page.scss'],
 })
 export class Form02Page implements OnInit {
+  public qntdcig;
+  public valcig;
+  public qntdcigma;
+
+  public user = {
+    nome: ' ',
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public savecig() {
+    this.user.nome = this.qntdcig;
+  }
+  public savevalcig() {
+    this.user.nome = this.valcig;
+  }
+  public saveqntdcig() {
+    this.user.nome = this.qntdcigma;
+  }
+
+
+  logForm() {
+    this.user
   }
 
 }
