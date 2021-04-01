1ce20cde4ae3b0b66d2e819010ab0a9f27f9ca56
dan01silva <dmatos@braspag.com.br>
2021-03-31 22:46:32 -0300

    Develop Daniel

--- src/app/depoimentos/depoimentos.page.ts
+++ src/app/depoimentos/depoimentos.page.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./depoimentos.page.scss'],
 })
 export class DepoimentosPage implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
   }
+  public depoiments() {
+    var depoimento01 = 'Comecei a fumar influenciado pelos amigos da escola.Eram cigarros com cheiros e gostos atrativos, como gudang, cigarros mentolados.Quando vi, já era viciado.Aos 27 anos, fumava três carteiras de cigarro por dia.Gastava em torno de R$ 320 por mês.Não tinha mais fôlego para subir escadas, minhas roupas ficavam manchadas, pois eu transpirava cigarro.Minha saúde ficou debilitada.Um dia, precisei vir ao Pama 1(bairro Czerniewicz) e vi um cartaz sobre o Programa Municipal de Controle do Tabagismo.Subi as escadas, quase sem fôlego, e perguntei sobre o programa.A coordenadora disse que ia me ligar quando fosse aberto um novo grupo.Não imaginei que realmente fossem me ligar, mas ligaram.Sozinho, eu já tinha tentado parar.Quando acordava, pensava: "hoje não vou fumar".Mas a vontade não durava mais que meia hora e eu já me pegava fumando.Ao mesmo tempo, não me conformava que o cigarro estava sendo mais forte que eu.Comecei a participar do programa, usei adesivo de nicotina, antidepressivo e estipulei uma data para parar de fumar.A partir daquele dia, nunca mais fumei.Já faz um ano.Foi no dia 15 de maio de 2013';
 
+  };
 }
