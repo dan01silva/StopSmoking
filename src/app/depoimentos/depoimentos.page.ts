4997ae9c62e67204362731e8ba62a1f5b42da080
dan01silva <dmatos@braspag.com.br>
2021-04-26 15:51:51 -0300

    config lounch.json

--- src/app/depoimentos/depoimentos.page.ts
+++ src/app/depoimentos/depoimentos.page.ts
@@ -9,18 +9,22 @@ export class DepoimentosPage implements OnInit {
 
   slides = [
     {
+      titulo: "Cláudia, 32 anos",
+      data: "11/06/2021",
+      depoimento: "Me chamo Cláudia e fumo desde os meus 17 anos, conheci o App StopSmoking e já estou 3 meses sem fumar, o app realmente tem me ajudado!",
+      
+    },
+    {
+      data: "11/06/2021",
+      depoimento: "Estou 15 dias sem fumar, o app tem me ajudado muito, já cheguei a fumar 2 maços de cigarro por dia, valeu muito apena  ter baixado o app. <br> Antônio Mendonça, 45 anos",
+    },
+    {
       titulo: "Leonardo Ferreira Gulpz",
       data: "11/06/2021",
       depoimento: "The Ionic Component Documentation showcases a number of useful components that are included out of the box with Ionic.",
-      imgAvt: "https://images.pexels.com/photos/697509/pexels-photo-697509.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-    },
-    {
-      titulo: "Danzinws",
-      data: "02/06/2019",
-      depoimento: "The <b>Ionic Cloud</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
-      imgAvt: "assets/img/ica-slidebox-img-3.png",
-    }
-  ];
+    },  
+  ]   
+  
   constructor() { }
 
   public homepage = "http://localhost:8100/home"
