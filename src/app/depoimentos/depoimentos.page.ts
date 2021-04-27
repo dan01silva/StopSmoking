3881b39b7a57556d3f416591d5dfcb85769786b9
dan01silva <dmatos@braspag.com.br>
2021-04-26 23:25:04 -0300

    Co-authored-by: Daniel Silva <60281506+dan01silva@users.noreply.github.com>

--- src/app/depoimentos/depoimentos.page.ts
+++ src/app/depoimentos/depoimentos.page.ts
@@ -9,13 +9,14 @@ export class DepoimentosPage implements OnInit {
 
   slides = [
     {
-      titulo: "Cláudia, 32 anos",
       data: "11/06/2021",
+      titulo: "Cláudia, 32 anos",
       depoimento: "Me chamo Cláudia e fumo desde os meus 17 anos, conheci o App StopSmoking e já estou 3 meses sem fumar, o app realmente tem me ajudado!",
       
     },
     {
       data: "11/06/2021",
+      titulo: "Thiago Felipe, 22",
       depoimento: "Estou 15 dias sem fumar, o app tem me ajudado muito, já cheguei a fumar 2 maços de cigarro por dia, valeu muito apena  ter baixado o app. <br> Antônio Mendonça, 45 anos",
     },
     {
