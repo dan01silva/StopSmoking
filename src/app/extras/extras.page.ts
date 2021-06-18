08467d89f46814bcb3a31defa9f72b329bfaffa7
Mauricio Oliveira <mauricioboliveira@gmail.com>
2021-06-18 17:42:05 -0300

    PÁGINA DE NOTICIAS

--- src/app/extras/extras.page.ts
+++ src/app/extras/extras.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Plugins } from '@capacitor/core';
 
 @Component({
   selector: 'app-extras',
@@ -9,7 +10,12 @@ export class ExtrasPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    
+
+const { Browser } = Plugins;
+
+await Browser.open({ url: 'https://www.google.com/search?q=pare+de+fumar&sxsrf=ALeKk03jikLvv2C8CZZZYVb8qmYFpcsBBA:1624048591730&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjBkKG7hKLxAhUCppUCHVdCA2gQ_AUoBHoECAEQBg&biw=1920&bih=937' });
   }
 
 }
